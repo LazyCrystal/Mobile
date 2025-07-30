cd78097aa00f6e5b4e0cf578e1b3e3133893c270
Sim <simjq-pm22@student.tarc.edu.my>
2025-07-30 18:25:04 +0800

    Three File Changes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        ndkVersion = "27.0.12077973"
     }
 
     buildTypes {
--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,12 @@ import 'home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
